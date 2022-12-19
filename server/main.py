8cb88e98d9a7e811eb5c39a46f3ae144228320b0
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2022-12-19 02:01:47 +0100

    Revert "commit"
    This reverts commit 8d8f6b0516760c9f223dfda034c8e046b8c8bf6c.

--- server/main.py
+++ server/main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from controllers.affichage_cycle import affichage_cycle
 from controllers.affichage_planning_after import affichage_planning_after
 from controllers.affichage_attestation import affichage_attestation
-from controllers.relever_de_note import affichage_relever_de_note
 
 app = Flask(__name__, template_folder='templates')
 
@@ -23,12 +22,10 @@ def planning_after():
 def attestation():
     affichage_attestation()
     return render_template('attestation.html', template_folder='templates')
-
-
-@app.route('/relever_de_note')
-def relever_de_note():
-        affichage_relever_de_note()
-        return render_template('relever_de_note.html', template_folder='templates')
+# @app.route('/carte_etudiant_back')
+# def carte_etudiant_back():
+#         affichage_carte_etudiant_back()
+#         return render_template('carte_etudiant_back.html', template_folder='templates')
 
 
 if __name__ == '__main__':
